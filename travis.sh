28e1b5661b05939f08e34c08dac64809ee06daf4
Tom <thomas.verin@sonarsource.com>
2016-03-07 13:49:14 +0100

    setup QA with test duplication from travis

--- travis.sh
+++ travis.sh
@@ -2,31 +2,11 @@
 
 set -euo pipefail
 
-function installTravisTools {
+function configureTravis {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v21 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v27 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
+configureTravis
 
-case "$TEST" in
-
-ci)
-  mvn verify -B -e -V
-  ;;
-
-plugin|ruling|type-inference)
-  installTravisTools
-
-  if [ "$SQ_VERSION" = "DEV" ] ; then
-    build_snapshot "SonarSource/sonarqube"
-  fi
-
-  mvn -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false -Pit-$TEST package
-  ;;
-
-*)
-  echo "Unexpected TEST mode: $TEST"
-  exit 1
-  ;;
-
-esac
+regular_mvn_build_deploy_analyze    
